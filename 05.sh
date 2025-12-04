cfab26662eed538b43e126e0e623791dd1d5e572
suresh vaadapalli <surehs@gmail.com>
2025-12-04 23:07:58 +0530

    sr

--- 05.sh
+++ 05.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo $($?)
+USERID=$(id -u)
+
+if [ $USERID == 0 ]; then
+  echo "please run with sudo user"
+  exit
+else 
+  echo "have sudo access"
+
+VALIDATE(){
+    if [ $1 -ne 0]; then
+    echo "error: installing $2 is failure"
+    else
+    echo "$2 installing success"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
